fdb449b92e99b8510cb7378c4fcace418275dc74
Heber F Barra <heberferrirabarra@gmail.com>
2023-09-25 09:54:41 -0300

    Classe para controle de cada usuário

--- src/main/java/com/trabalho/programacao/Usuario.java
+++ src/main/java/com/trabalho/programacao/Usuario.java
@@ -1,168 +1,74 @@
 package com.trabalho.programacao;
 
-import java.util.Arrays;
-import java.util.HashMap;
+
 import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Calendar;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JPasswordField;
-import javax.swing.JOptionPane;
 
 public class Usuario {
 
-    private static final String VALOR_REFAZER = "refazer", CANCELAR_OPERACAO = "cancelar";
-    private static final List<String> VALORES_PROIBIDOS = Arrays.stream(new String[]{"", VALOR_REFAZER, CANCELAR_OPERACAO}).toList();
+    private static String usuarioAtual;
+    protected LinkedHashMap<String, String> infoUsuario;
 
-    public static String confirmarEscolha(String valor, String nomeValor, String tituloValor) {
-        while (true) {
-            int escolha = JOptionPane.showConfirmDialog(
-                    null,
-                    nomeValor + " " + valor + "\nEstá certo?",
-                    tituloValor,
-                    JOptionPane.YES_NO_CANCEL_OPTION
-            );
-
-            switch (escolha) {
-                case 0 -> {
-                    return valor;
-                }
-                case 1 -> {
-                    return VALOR_REFAZER;
-                }
-                case -1, 2 -> {
-                    if (deveCancelarOperacao()) return CANCELAR_OPERACAO;
-                }
-            }
-        }
+    public Usuario(LinkedHashMap<String, String> info) {
+        this.infoUsuario = info;
     }
 
-    private static boolean verificarValorString(String valor, String nomeValor) {
-        if (VALORES_PROIBIDOS.contains(valor)) {
-            JOptionPane.showMessageDialog(
-                    null,
-                    nomeValor + " inválido(a)",
-                    "Aviso",
-                    JOptionPane.WARNING_MESSAGE
-            );
-
-            return true;
-        }
-
-        return false;
+    public static void setUsuarioAtual(String nomeUsuario) {
+        usuarioAtual = nomeUsuario;
     }
 
-    private static boolean deveCancelarOperacao() {
-        return JOptionPane.showConfirmDialog(
-                null,
-                "Deseja cancelar a operação? ",
-                "Cancelar",
-                JOptionPane.YES_NO_OPTION
-        ) == 0;
+    public static String getUsuarioAtual() {
+        return  usuarioAtual;
     }
 
-    private static String criarNomeDeUsuario() {
-        String nome;
-
-        while (true) {
-
-            try {
-                nome = JOptionPane.showInputDialog("Nome de usuário: ").strip();
-            } catch (NullPointerException e) {
-                if (deveCancelarOperacao()) return CANCELAR_OPERACAO;
-                continue;
-            }
-
-            if (verificarValorString(nome, "Nome de usuário")) continue;
-            nome = confirmarEscolha(nome, "Nome de usuário", "Nome de Usuário");
-            if (nome.equals(VALOR_REFAZER)) continue;
-
-            return nome;
-        }
+    public String getNome() {
+        return this.infoUsuario.getOrDefault("nome", "");
     }
 
-    private static String criarSenha() {
-        var jPanel = new JPanel();
-        var labelSenha = new JLabel("Digite uma senha:");
-        var fieldSenha = new JPasswordField(10);
-        var labelConfirmarSenha = new JLabel("Redigite a senha: ");
-        var fieldConfirmarSenha = new JPasswordField(10);
-
-        jPanel.add(labelSenha);
-        jPanel.add(fieldSenha);
-        jPanel.add(labelConfirmarSenha);
-        jPanel.add(fieldConfirmarSenha);
-
-        while (true) {
-
-            int escolha = JOptionPane.showConfirmDialog(
-                    null,
-                    jPanel,
-                    "Senha",
-                    JOptionPane.OK_CANCEL_OPTION
-            );
-
-            if ((escolha == -1 || escolha == 2) && deveCancelarOperacao()) {
-                return CANCELAR_OPERACAO;
-            }
-
-            if (verificarValorString(String.valueOf(fieldSenha.getPassword()), "Senha")) {
-                continue;
-            }
-
-            if (!(Arrays.equals(fieldSenha.getPassword(), fieldConfirmarSenha.getPassword()))) {
-                JOptionPane.showMessageDialog(
-                        null,
-                        "Senhas são diferentes! Por favor digite novamente.",
-                        "Aviso",
-                        JOptionPane.WARNING_MESSAGE
-                );
-
-                continue;
-            }
-
-            return String.valueOf(fieldSenha.getPassword());
-        }
+    public String getHashSenha() {
+        return this.infoUsuario.getOrDefault("senha", "");
     }
 
+    public byte[] getSalt() {
+        String saltString = this.infoUsuario.getOrDefault("salt", "");
+        byte[] salt = new byte[16];
 
-    public static void criarUsuario() {
-        String nome, senha = "", salt;
+        if (saltString.isEmpty()) return new byte[0];
 
-        nome = criarNomeDeUsuario();
-        if (!nome.equals(CANCELAR_OPERACAO)) senha = criarSenha();
+        String[] partesSalt = saltString.replace("[", "").replace("]", "").split(", ");
 
-        if (nome.equals(CANCELAR_OPERACAO) || senha.equals(CANCELAR_OPERACAO)) {
-            JOptionPane.showMessageDialog(
-                    null,
-                    "Operação cancelada",
-                    "Cancelada",
-                    JOptionPane.INFORMATION_MESSAGE
-            );
-
-            return;
+        for (int i = 0; i < partesSalt.length; i++) {
+            salt[i] = (byte) (Integer.parseInt(partesSalt[i]));
         }
 
-        String[] resultado = DataHash.dataHasher(senha);
-
-        senha = resultado[0];
-        salt = resultado[1];
-
-        HashMap<String, String> usuario = new LinkedHashMap<>();
-
-        usuario.put("nome", nome);
-        usuario.put("senha", senha);
-        usuario.put("salt", salt);
-        usuario.put("highscore", "0");
-        usuario.put("modo de jogo (hscore)", null);
-        usuario.put("quantidade de jogos", "0");
-        usuario.put("data de criação", String.valueOf(Calendar.getInstance().get(Calendar.DATE)));
-
-
-
-        for (String key: usuario.keySet()) {
-            System.out.println(key + ": " + usuario.get(key));
-        }
+        return salt;
     }
+
+    public int getHighScore() {
+        return Integer.parseInt(this.infoUsuario.getOrDefault("highscore", "0"));
+    }
+
+    public void setHighScore(Integer novoHighScore) {
+        this.infoUsuario.replace("highscore", String.valueOf(novoHighScore));
+    }
+
+    public String getModoDeJogo() {
+        return this.infoUsuario.getOrDefault("modo de jogo (hscore)", "");
+    }
+
+    public void setModoDeJogo(String modoDeJogo) {
+        this.infoUsuario.replace("modo de jogo(hscore)", modoDeJogo);
+    }
+
+    public int getQuantidadeDeJogos() {
+        return Integer.parseInt(this.infoUsuario.getOrDefault("quantidade de jogos", "0"));
+    }
+
+    public void aumentarQuantidadeDeJogos() {
+        this.infoUsuario.replace("quantidade jogos", String.valueOf(getQuantidadeDeJogos() + 1));
+    }
+
+    public static boolean compararSenha(String hashSenha, String senha, byte[] salt) {
+        return hashSenha.equals(DataHash.dataHasher(senha, salt));
+    }
+
 }
